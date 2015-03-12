58de34c3e647a293be63ac8e12e03c0efd0a4a02
David <670082622@qq.com>
2015-03-12 11:23:50 +0800

    no message

---
+++ ad12/ad12/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ad12
+//
+//  Created by David on 15/3/12.
+//  Copyright (c) 2015年 novaloncn.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
